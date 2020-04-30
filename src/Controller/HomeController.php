07d06882c9db4cef2cae2e44c521e0f1cffe56b5
mohakhlf <mohakhellafi@gmail.com>
2020-04-30 09:36:01 +0200

    add moha part

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -70,5 +70,58 @@ class HomeController extends AbstractController
         ];
         return $this->twig->render('Home/View.html.twig', ["produit"=>$produit]);
     }
+
+//    /**
+//     * Display home page
+//     *
+//     * @return string
+//     * @throws \Twig\Error\LoaderError
+//     * @throws \Twig\Error\RuntimeError
+//     * @throws \Twig\Error\SyntaxError
+//     */
+//    public function index()
+//    {
+//        $client = HttpClient::create();
+//        $response = $client->request("GET", "https://collectionapi.metmuseum.org/public/collection/v1/search?medium=Paintings&hasImages=true&q=sunset");
+//        $object1 = $client->request("GET", "https://collectionapi.metmuseum.org/public/collection/v1/objects/437980");
+//        $object2 = $client->request("GET", "https://collectionapi.metmuseum.org/public/collection/v1/objects/437980");
+//        $object3 = $client->request("GET", "https://collectionapi.metmuseum.org/public/collection/v1/objects/437980");
+//        $object4 = $client->request("GET", "https://collectionapi.metmuseum.org/public/collection/v1/objects/437980");
+//
+//
+//        $statusCode = $response->getStatusCode(); // get Response status code 200
+//
+//        if ($statusCode === 200) {
+//            $content = $response->getContent();
+////             get the response in JSON format
+//            $content = $response->toArray();
+//            $content1 = $object1->toArray();
+//            $content2 = $object2->toArray();
+//            $content3 = $object3->toArray();
+//            $content4 = $object4->toArray();
+////             convert the response (here in JSON) to an PHP array
+//            var_dump($content["objectIDs"]);
+////            var_dump($content1);
+////            var_dump($content2);
+////            var_dump($content3);
+////            var_dump($content4);
+//        }
+//
+//
+//        return $this->twig->render('Home/index.html.twig', ["content" => $content, "collection1"=>$content1, "collection2"=>$content2, "collection3"=>$content3, "collection4"=>$content4]);
+//    }
+//}
+
+//    public function fetchByOne($code)
+//    {
+//        $client = HttpClient::create();
+//        $objectM = $client->request("GET", "https://collectionapi.metmuseum.org/public/collection/v1/objects/$code");
+//
+//        $statusCodeM = $objectM->getStatusCode();
+//
+//        var_dump($statusCodeM);
+//
+//    }
+//}
 }
 
